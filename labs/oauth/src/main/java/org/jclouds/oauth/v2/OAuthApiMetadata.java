jclouds/legacy-jclouds

clean slate commit with the first oauth v2 impl. includes corrections from comments from mattsptep made in previous pr

labs/oauth/src/main/java/org/jclouds/oauth/v2/OAuthApiMetadata.java
@@ -1,0 +1,95 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.TypeToken;
+import com.google.inject.Module;
+import org.jclouds.apis.ApiMetadata;
+import org.jclouds.oauth.v2.config.OAuthRestClientModule;
+import org.jclouds.rest.RestContext;
+import org.jclouds.rest.internal.BaseRestApiMetadata;
+
+import java.net.URI;
+import java.util.Properties;
+
+import static org.jclouds.oauth.v2.OAuthConstants.PKCS_CERITIFICATE_KEY_PASSWORD;
+import static org.jclouds.oauth.v2.OAuthConstants.PKCS_CERTIFICATE_KEY_NAME;
+import static org.jclouds.oauth.v2.OAuthConstants.SIGNATURE_ALGORITHM;
+import static org.jclouds.oauth.v2.OAuthConstants.SIGNATURE_KEY_FORMAT;
+
+/**
+ * Implementation of {@link ApiMetadata} for OAuth 2 API
+ *
+ * @author David Alves
+ */
+public class OAuthApiMetadata extends BaseRestApiMetadata {
+
+   public static final TypeToken<RestContext<OAuthClient, OAuthAsyncClient>> CONTEXT_TOKEN = new
+           TypeToken<RestContext<OAuthClient, OAuthAsyncClient>>() {};
+
+   @Override
+   public Builder toBuilder() {
+      return new Builder().fromApiMetadata(this);
+   }
+
+   public OAuthApiMetadata() {
+      this(new Builder());
+   }
+
+   protected OAuthApiMetadata(Builder builder) {
+      super(builder);
+   }
+
+   public static Properties defaultProperties() {
+      Properties properties = BaseRestApiMetadata.defaultProperties();
+      properties.put(SIGNATURE_ALGORITHM, "RS256");
+      properties.put(SIGNATURE_KEY_FORMAT, "PKCS12");
+      properties.put(PKCS_CERTIFICATE_KEY_NAME, "privatekey");
+      properties.put(PKCS_CERITIFICATE_KEY_PASSWORD, "notasecret");
+      return properties;
+   }
+
+   public static class Builder extends BaseRestApiMetadata.Builder {
+
+      protected Builder() {
+         super(OAuthClient.class, OAuthAsyncClient.class);
+         id("bla").name("OAuth API")
+                 .identityName("service_account")
+                 .credentialName("service_key")
+                 .documentation(URI.create("TODO"))
+                 .version("2")
+                 .defaultProperties(OAuthApiMetadata.defaultProperties())
+                 .defaultModules(ImmutableSet.<Class<? extends Module>>of(OAuthRestClientModule.class));
+      }
+
+      @Override
+      public OAuthApiMetadata build() {
+         return new OAuthApiMetadata(this);
+      }
+
+      @Override
+      public Builder fromApiMetadata(ApiMetadata in) {
+         super.fromApiMetadata(in);
+         return this;
+      }
+
+   }
+
+}

labs/oauth/src/main/java/org/jclouds/oauth/v2/OAuthConstants.java
@@ -1,0 +1,65 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2;
+
+/**
+ * The constants for OAuth \
+ *
+ * @author David Alves
+ */
+public class OAuthConstants {
+
+   /**
+    * The selected signature algorithm to use to sign the requests.
+    */
+   public static final String SIGNATURE_ALGORITHM = "jclouds.bla.signature-algo";
+
+   /**
+    * The format of the certificate key file that will be used to sign the token request.
+    * Supported formats are PKCS12 and PKCS8, default is PKCS12
+    */
+   public static final String SIGNATURE_KEY_FORMAT = "jclouds.bla.signature-key-format";
+
+   /**
+    * The name of the key (if the ceritificate is in a PKCS12 Keystore)
+    */
+   public static final String PKCS_CERTIFICATE_KEY_NAME = "jclouds.bla.pkcs12.key-name";
+
+   /**
+    * The the password for the key (if the ceritificate is in a PKCS12 Keystore)
+    */
+   public static final String PKCS_CERITIFICATE_KEY_PASSWORD = "jclouds.bla.pkcs12.key-password";
+
+   /**
+    * The permissions being requested.
+    */
+   public static final String TOKEN_SCOPE = "jclouds.bla.scope";
+
+   /**
+    * The assertion target
+    */
+   public static final String TOKEN_ASSERTION_DESCRIPTION = "jclouds.bla.assertion-description";
+
+   /**
+    * The (optional) set of additional claims to use, provided in Map<String,String> form
+    */
+   public static final String ADDITIONAL_CLAIMS = "jclouds.bla.additional-claims";
+
+
+}

labs/oauth/src/main/java/org/jclouds/oauth/v2/config/OAuthAuthenticationModule.java
@@ -1,0 +1,60 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2.config;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import org.jclouds.oauth.v2.OAuthAsyncClient;
+import org.jclouds.oauth.v2.OAuthClient;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.net.URI;
+
+import static org.jclouds.rest.config.BinderUtils.bindClientAndAsyncClient;
+
+/**
+ * Configures the OAuth connection.
+ *
+ * @author David Alves
+ */
+public class OAuthAuthenticationModule extends AbstractModule {
+
+   @Override
+   protected void configure() {
+      install(new OAuthBaseModule());
+      // AuthenticationApi is used directly for filters and retry handlers, so let's bind it
+      // explicitly
+      bindClientAndAsyncClient(binder(), OAuthClient.class, OAuthAsyncClient.class);
+   }
+
+   /**
+    * When bla is used as a module the bla endpoint is a normal property
+    */
+   @Provides
+   @Singleton
+   @Authentication
+   protected Supplier<URI> provideAuthenticationEndpoint(@Named("bla.endpoint") String endpoint) {
+      return Suppliers.ofInstance(URI.create(endpoint));
+   }
+
+
+}

labs/oauth/src/test/java/org/jclouds/oauth/v2/OAuthTestUtils.java
@@ -1,0 +1,19 @@
+package org.jclouds.oauth.v2;
+
+import java.util.Properties;
+
+import static org.jclouds.oauth.v2.OAuthConstants.TOKEN_ASSERTION_DESCRIPTION;
+import static org.jclouds.oauth.v2.OAuthConstants.TOKEN_SCOPE;
+
+public class OAuthTestUtils {
+
+   public static Properties defaultProperties(Properties properties) {
+      properties = properties == null ? new Properties() : properties;
+      properties.put("bla.identity", "foo");
+      properties.put("bla.credential", "target/test-classes/test.p12");
+      properties.put("bla.endpoint", "http://localhost:5000/o/oauth2/token");
+      properties.put(TOKEN_SCOPE, "https://www.googleapis.com/auth/prediction");
+      properties.put(TOKEN_ASSERTION_DESCRIPTION, "https://accounts.google.com/o/oauth2/token");
+      return properties;
+   }
+}

labs/oauth/src/test/java/org/jclouds/oauth/v2/internal/BaseOAuthApiLiveTest.java
@@ -1,0 +1,44 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2.internal;
+
+import com.google.common.reflect.TypeToken;
+import org.jclouds.apis.BaseContextLiveTest;
+import org.jclouds.oauth.v2.OAuthApiMetadata;
+import org.jclouds.oauth.v2.OAuthAsyncClient;
+import org.jclouds.oauth.v2.OAuthClient;
+import org.jclouds.rest.RestContext;
+
+
+/**
+ * @author Adrian Cole
+ */
+public class BaseOAuthApiLiveTest extends BaseContextLiveTest<RestContext<OAuthClient, OAuthAsyncClient>> {
+
+   public BaseOAuthApiLiveTest() {
+      provider = "bla";
+   }
+   
+   @Override
+   protected TypeToken<RestContext<OAuthClient, OAuthAsyncClient>> contextType() {
+      return OAuthApiMetadata.CONTEXT_TOKEN;
+   }
+
+}
+

labs/oauth/src/test/java/org/jclouds/oauth/v2/internal/BaseOAuthExpectTest.java
@@ -1,0 +1,31 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2.internal;
+
+import org.jclouds.rest.internal.BaseRestApiExpectTest;
+
+/**
+ * @author Adrian Cole
+ */
+public class BaseOAuthExpectTest<T> extends BaseRestApiExpectTest<T> {
+
+   public BaseOAuthExpectTest() {
+      provider = "bla";
+   }
+}

dralves/jclouds

labs/oauth/src/main/java/org/jclouds/oauth/v2/OAuthApiMetadata.java
@@ -1,0 +1,95 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.TypeToken;
+import com.google.inject.Module;
+import org.jclouds.apis.ApiMetadata;
+import org.jclouds.oauth.v2.config.OAuthRestClientModule;
+import org.jclouds.rest.RestContext;
+import org.jclouds.rest.internal.BaseRestApiMetadata;
+
+import java.net.URI;
+import java.util.Properties;
+
+import static org.jclouds.oauth.v2.OAuthConstants.PKCS_CERITIFICATE_KEY_PASSWORD;
+import static org.jclouds.oauth.v2.OAuthConstants.PKCS_CERTIFICATE_KEY_NAME;
+import static org.jclouds.oauth.v2.OAuthConstants.SIGNATURE_ALGORITHM;
+import static org.jclouds.oauth.v2.OAuthConstants.SIGNATURE_KEY_FORMAT;
+
+/**
+ * Implementation of {@link ApiMetadata} for OAuth 2 API
+ *
+ * @author David Alves
+ */
+public class OAuthApiMetadata extends BaseRestApiMetadata {
+
+   public static final TypeToken<RestContext<OAuthClient, OAuthAsyncClient>> CONTEXT_TOKEN = new
+           TypeToken<RestContext<OAuthClient, OAuthAsyncClient>>() {};
+
+   @Override
+   public Builder toBuilder() {
+      return new Builder().fromApiMetadata(this);
+   }
+
+   public OAuthApiMetadata() {
+      this(new Builder());
+   }
+
+   protected OAuthApiMetadata(Builder builder) {
+      super(builder);
+   }
+
+   public static Properties defaultProperties() {
+      Properties properties = BaseRestApiMetadata.defaultProperties();
+      properties.put(SIGNATURE_ALGORITHM, "RS256");
+      properties.put(SIGNATURE_KEY_FORMAT, "PKCS12");
+      properties.put(PKCS_CERTIFICATE_KEY_NAME, "privatekey");
+      properties.put(PKCS_CERITIFICATE_KEY_PASSWORD, "notasecret");
+      return properties;
+   }
+
+   public static class Builder extends BaseRestApiMetadata.Builder {
+
+      protected Builder() {
+         super(OAuthClient.class, OAuthAsyncClient.class);
+         id("oauth").name("OAuth API")
+                 .identityName("service_account")
+                 .credentialName("service_key")
+                 .documentation(URI.create("TODO"))
+                 .version("2")
+                 .defaultProperties(OAuthApiMetadata.defaultProperties())
+                 .defaultModules(ImmutableSet.<Class<? extends Module>>of(OAuthRestClientModule.class));
+      }
+
+      @Override
+      public OAuthApiMetadata build() {
+         return new OAuthApiMetadata(this);
+      }
+
+      @Override
+      public Builder fromApiMetadata(ApiMetadata in) {
+         super.fromApiMetadata(in);
+         return this;
+      }
+
+   }
+
+}

labs/oauth/src/main/java/org/jclouds/oauth/v2/OAuthConstants.java
@@ -1,0 +1,65 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2;
+
+/**
+ * The constants for OAuth \
+ *
+ * @author David Alves
+ */
+public class OAuthConstants {
+
+   /**
+    * The selected signature algorithm to use to sign the requests.
+    */
+   public static final String SIGNATURE_ALGORITHM = "jclouds.oauth.signature-algo";
+
+   /**
+    * The format of the certificate key file that will be used to sign the token request.
+    * Supported formats are PKCS12 and PKCS8, default is PKCS12
+    */
+   public static final String SIGNATURE_KEY_FORMAT = "jclouds.oauth.signature-key-format";
+
+   /**
+    * The name of the key (if the ceritificate is in a PKCS12 Keystore)
+    */
+   public static final String PKCS_CERTIFICATE_KEY_NAME = "jclouds.oauth.pkcs12.key-name";
+
+   /**
+    * The the password for the key (if the ceritificate is in a PKCS12 Keystore)
+    */
+   public static final String PKCS_CERITIFICATE_KEY_PASSWORD = "jclouds.oauth.pkcs12.key-password";
+
+   /**
+    * The permissions being requested.
+    */
+   public static final String TOKEN_SCOPE = "jclouds.oauth.scope";
+
+   /**
+    * The assertion target
+    */
+   public static final String TOKEN_ASSERTION_DESCRIPTION = "jclouds.oauth.assertion-description";
+
+   /**
+    * The (optional) set of additional claims to use, provided in Map<String,String> form
+    */
+   public static final String ADDITIONAL_CLAIMS = "jclouds.oauth.additional-claims";
+
+
+}

labs/oauth/src/main/java/org/jclouds/oauth/v2/config/OAuthAuthenticationModule.java
@@ -1,0 +1,60 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2.config;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import org.jclouds.oauth.v2.OAuthAsyncClient;
+import org.jclouds.oauth.v2.OAuthClient;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.net.URI;
+
+import static org.jclouds.rest.config.BinderUtils.bindClientAndAsyncClient;
+
+/**
+ * Configures the OAuth connection.
+ *
+ * @author David Alves
+ */
+public class OAuthAuthenticationModule extends AbstractModule {
+
+   @Override
+   protected void configure() {
+      install(new OAuthBaseModule());
+      // AuthenticationApi is used directly for filters and retry handlers, so let's bind it
+      // explicitly
+      bindClientAndAsyncClient(binder(), OAuthClient.class, OAuthAsyncClient.class);
+   }
+
+   /**
+    * When oauth is used as a module the oauth endpoint is a normal property
+    */
+   @Provides
+   @Singleton
+   @Authentication
+   protected Supplier<URI> provideAuthenticationEndpoint(@Named("oauth.endpoint") String endpoint) {
+      return Suppliers.ofInstance(URI.create(endpoint));
+   }
+
+
+}

labs/oauth/src/test/java/org/jclouds/oauth/v2/OAuthTestUtils.java
@@ -1,0 +1,19 @@
+package org.jclouds.oauth.v2;
+
+import java.util.Properties;
+
+import static org.jclouds.oauth.v2.OAuthConstants.TOKEN_ASSERTION_DESCRIPTION;
+import static org.jclouds.oauth.v2.OAuthConstants.TOKEN_SCOPE;
+
+public class OAuthTestUtils {
+
+   public static Properties defaultProperties(Properties properties) {
+      properties = properties == null ? new Properties() : properties;
+      properties.put("oauth.identity", "foo");
+      properties.put("oauth.credential", "target/test-classes/test.p12");
+      properties.put("oauth.endpoint", "http://localhost:5000/o/oauth2/token");
+      properties.put(TOKEN_SCOPE, "https://www.googleapis.com/auth/prediction");
+      properties.put(TOKEN_ASSERTION_DESCRIPTION, "https://accounts.google.com/o/oauth2/token");
+      return properties;
+   }
+}

labs/oauth/src/test/java/org/jclouds/oauth/v2/internal/BaseOAuthApiLiveTest.java
@@ -1,0 +1,44 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2.internal;
+
+import com.google.common.reflect.TypeToken;
+import org.jclouds.apis.BaseContextLiveTest;
+import org.jclouds.oauth.v2.OAuthApiMetadata;
+import org.jclouds.oauth.v2.OAuthAsyncClient;
+import org.jclouds.oauth.v2.OAuthClient;
+import org.jclouds.rest.RestContext;
+
+
+/**
+ * @author Adrian Cole
+ */
+public class BaseOAuthApiLiveTest extends BaseContextLiveTest<RestContext<OAuthClient, OAuthAsyncClient>> {
+
+   public BaseOAuthApiLiveTest() {
+      provider = "oauth";
+   }
+   
+   @Override
+   protected TypeToken<RestContext<OAuthClient, OAuthAsyncClient>> contextType() {
+      return OAuthApiMetadata.CONTEXT_TOKEN;
+   }
+
+}
+

labs/oauth/src/test/java/org/jclouds/oauth/v2/internal/BaseOAuthExpectTest.java
@@ -1,0 +1,31 @@
+/**
+ * Licensed to jclouds, Inc. (jclouds) under one or more
+ * contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  jclouds licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jclouds.oauth.v2.internal;
+
+import org.jclouds.rest.internal.BaseRestApiExpectTest;
+
+/**
+ * @author Adrian Cole
+ */
+public class BaseOAuthExpectTest<T> extends BaseRestApiExpectTest<T> {
+
+   public BaseOAuthExpectTest() {
+      provider = "oauth";
+   }
+}